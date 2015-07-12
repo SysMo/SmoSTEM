0f26475e399d5d349369e83b24c9b02b768efcbb
Atanas Pavlov <atanas.pavlov@sysmoltd.com>
2015-07-12 16:42:48 +0300

    Implemented numerical input directive
    Signed-off-by: Atanas Pavlov <atanas.pavlov@sysmoltd.com>

--- static/js/directives/CoreDirectives.js
+++ static/js/directives/CoreDirectives.js
@@ -8,7 +8,7 @@ Stem.directive('stemListItemActions', [function() {
 			moveUp: "&moveUp",
 			moveDown: "&moveDown",
 		},
-		templateUrl: "stem-list-item-actions",
+		templateUrl: "stem-list-item-actions.html",
 		link: function(scope, element, attributes) {
 			if (!("add" in attributes)) {
 				scope.add = false;
@@ -28,6 +28,30 @@ Stem.directive('stemListItemActions', [function() {
 			if (!("moveDown" in attributes)) {
 				scope.moveDown = false;
 			}
+		}
+	}
+}]);
+
+Stem.directive('stemInputNumerical', [function() {
+	return {
+		scope: {
+			numValue:'=stemInputNumerical'
+		},
+		restrict: 'A',
+		templateUrl: "stem-input-numerical.html",
+		controller: function($scope, StemUtil) {
+			$scope.updateDisplay = function() {
+				$scope.displayValue = StemUtil.formatNumber($scope.numValue);
+			}
+			$scope.onInputChange = function() {
+				var numValue = parseFloat($scope.displayValue);
+				if (!isNaN(numValue)) {
+					$scope.numValue = numValue;
+				}
+			}
+			$scope.updateDisplay();
+		},
+		link: function(scope, element, attributes) {
 		}
 	}
 }]);
